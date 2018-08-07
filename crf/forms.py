29700d633cf81d9bdc82429ebb4b5e5687cf6790
Carlos Jimenez Alvarado <jimenc33@chl-wsk01.pfizer.com>
2018-08-07 08:44:06 -0700

    quito validación unicidad a subjectId

--- crf/forms.py
+++ crf/forms.py
@@ -7,3 +7,18 @@ class SubjectForm(forms.ModelForm):
     class Meta:
         model = Subject
         fields = ('subjectId', )
+
+    def validate_unique(self):
+        #De este modo se evita la validación de unicidad en crf/views.py:form.is_valid()
+        pass
+
+'''
+    def validate_unique(self):
+        #De este modo se evita la validación de unicidad en crf/views.py:form.is_valid()
+        return Subject.validate_unique( self )
+        pass
+
+    def validate_unique(self):
+        #return not Subject.objects.filter(subjectId=self.subjectId).exists()
+        return not 
+'''
--- crf/views.py
+++ crf/views.py
@@ -4,14 +4,12 @@ from django import forms
 from .forms import SubjectForm
 from .models import Subject
 
+from django.core.exceptions import ValidationError
+
 '''
 def crf_new(request):
     form = SubjectForm()
     return render(request, 'crf_edit.html', {'form': form})
-'''
-
-
-
 
 def crf_new(request):
     if request.method == "POST":
@@ -22,12 +20,42 @@ def crf_new(request):
             #post.author = request.user
             #post.published_date = timezone.now()
             #post.save()
-            '''
-            Previo a desplegar formsets
-            '''
             print('Previo a desplegar formsets')
             #return redirect('post_detail', pk=post.pk)
     else:
         form = SubjectForm()
     return render(request, 'crf_edit.html', {'form': form})
     #return render(request, 'blog/post_edit.html', {'form': form})
+
+def crf_newWORKING_ON(request):
+    if request.method == "POST":
+        form = SubjectForm(request.POST)
+        if form.is_valid():
+            subject = form.save(commit=False)
+            if Subject.objects.filter(subjectId=subject.subjectId).exists():
+                print('subjectId[{0}] existe'.format(subject.subjectId))
+                print('Previo a desplegar formsets')
+            else:
+                #raise ValidationError(_('No existe un SubjectID con este identificador.'))
+                raise ValidationError('No existe un SubjectID con este identificador.')
+    else:
+        form = SubjectForm()
+    return render(request, 'crf_edit.html', {'form': form})
+'''
+
+def crf_new(request):
+    if request.method == "POST":
+        form = SubjectForm(request.POST)
+        if form.is_valid():
+            subject = form.save(commit=False)
+            print('subjectId[{0}]'.format(subject.subjectId))
+            #post.author = request.user
+            #post.published_date = timezone.now()
+            #post.save()
+            print('Previo a desplegar formsets')
+            #return redirect('post_detail', pk=post.pk)
+    else:
+        form = SubjectForm()
+    return render(request, 'crf_edit.html', {'form': form})
+    #return render(request, 'blog/post_edit.html', {'form': form})
+

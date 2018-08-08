c07a2f7d5e5535c6c50c6f08eeb94a8c87b3ee1b
Carlos Jimenez Alvarado <jimenc33@chl-wsk01.pfizer.com>
2018-08-08 11:50:02 -0700

    populate_models working ok for Subject & Variation

--- crf/management/commands/populate_models.py
+++ crf/management/commands/populate_models.py
@@ -8,39 +8,34 @@ class Command(BaseCommand):
     help = 'our help string comes here'
 
     def _create_objs(self):
-        #Variation.objects.all().delete()
-        #Subject.objects.all().delete()
-        print("SUBJECTID_DATAFILE[{0}]\nVARIANT_DATAFILE[{1}]".format(SUBJECTID_DATAFILE, VARIANT_DATAFILE))
-        # Things to do:
-        # i) open file SUBJECTID_DATAFILE
-        # ii) read each its lines
-        # iii) create an object of kind Subject
-        # iv) save
-        #    >>> from crf.models import Subject
-        #    >>> s1 = Subject(subjectId='11223344')
-        #    >>> s1.save()
-
-        # Iterate over the lines of the file
-        #with open(SUBJECTID_DATAFILE, 'rt', newline='') as f:
+        #print("SUBJECTID_DATAFILE[{0}]\nVARIANT_DATAFILE[{1}]".format(SUBJECTID_DATAFILE, VARIANT_DATAFILE))
+        # Populate Subject
         subjetSet = set()
         with open(SUBJECTID_DATAFILE, 'rt') as f:
             for line in f:
-                # process line
-                print("subjectId[{0}]".format(line.strip()), end='\n')
-                # add values to a set to avoid duplicates
-                subjetSet.add( line.strip() )
-                '''
-                subject = Subject(subjectId='11223344')
-                subject.save()
-                '''
+                #print("subjectId[{0}]".format(line.strip()), end='\n')
+                subjetSet.add( line.strip() ) # add values to a set to avoid duplicates
 
         #pprint( subjetSet )
 
         for subjectId in subjetSet:
-            print("subjectId[{0}]".format(subjectId), end='\n')
+            #print("subjectId[{0}]".format(subjectId), end='\n')
             subject = Subject(subjectId=subjectId)
             subject.save()
 
+        # Populate Variation
+        variationSet = set()
+        with open(VARIANT_DATAFILE, 'rt') as f:
+            for line in f:
+                #print("_variation[{0}]".format(line.strip()), end='\n')
+                variationSet.add( line.strip() ) # add values to a set to avoid duplicates
+
+        #pprint( variationSet )
+
+        for _variation in variationSet:
+            #print("_variation[{0}]".format(_variation), end='\n')
+            variation = Variation(variation=_variation)
+            variation.save()
 
     def handle(self, *args, **options):
         self._create_objs()
